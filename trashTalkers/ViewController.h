a253ea933279acbde919253ca66382503a97d5ad
Matt Remick <mremick@gmail.com>
2013-11-15 18:35:54 -0800

    Initial Commit

---
+++ trashTalkers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  trashTalkers
+//
+//  Created by Matt Remick on 11/15/13.
+//  Copyright (c) 2013 Matt Remick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
