debb79295d5a5c8fc7802b9cb86ec484a2c0aedb
Matt Remick <mremick@gmail.com>
2013-12-09 17:33:24 -0800

    added a segue from the friends list to the show profile page to view your friends profile.

--- trashTalkers/FriendsViewController.h
+++ trashTalkers/FriendsViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <Parse/Parse.h>
+#import "UserProfileViewController.h"
 
 @interface FriendsViewController : UITableViewController
 
--- trashTalkers/FriendsViewController.m
+++ trashTalkers/FriendsViewController.m
@@ -127,16 +127,20 @@
 }
 */
 
-/*
+
 #pragma mark - Navigation
 
 // In a story board-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if ([segue.identifier isEqualToString:@"showUser"]) {
+        UserProfileViewController *vc = [segue destinationViewController];
+        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+        PFUser *friend = [self.friends objectAtIndex:indexPath.row];
+        vc.userName = friend.username;
+    }
 }
 
- */
+ 
 
 @end
